5efde8233556cf577d9a6832835dbedc9c3d02e0
ke-kaa <amsalukaku122@gmail.com>
2025-05-22 03:14:52 +0300

    Edit WebInfo component display

--- GithubProfileAnalyzer/src/components/WebInfo/WebInfo.jsx
+++ GithubProfileAnalyzer/src/components/WebInfo/WebInfo.jsx
@@ -10,15 +10,12 @@ export default function WebInfo() {
             <p className='center'>"Visualize GitHub repositories, languages, and stars in one click." </p>
 
             <div className='services-list'>
-
                 <p className='center'>Enter a Github username to:</p>
-
-                <p class="item-text"><span className='number-circle'>1</span>See a breakdown of programming languages used.</p>
-                
-                <p class="item-text"><span className='number-circle'>2</span>View stars per repository.</p>
-
-                <p class="item-text"><span className='number-circle'>3</span> Get a summary of their public Github activity.</p>
-
+                <div className='services'>
+                    <p class="item-text"><span className='number-circle'>1</span>See a breakdown of programming languages used.</p>
+                    <p class="item-text"><span className='number-circle'>2</span>View stars per repository.</p>
+                    <p class="item-text"><span className='number-circle'>3</span> Get a summary of their public Github activity.</p>
+                </div>
             </div>
         </div>
     )
