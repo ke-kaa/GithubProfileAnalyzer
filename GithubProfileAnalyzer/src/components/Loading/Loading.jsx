13a79e5d107b48d91976d21da2ee59d501d17bc7
ke-kaa <amsalukaku122@gmail.com>
2025-06-09 23:46:53 +0300

    Add Loading Component when searching user, update HomePage.jsx to handle error

---
+++ GithubProfileAnalyzer/src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Loading.css'
+
+export default function  Loading() {
+    return (
+        <div className='loader'>
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    )
+}
